fa653cbeff401ca027df2b9bd87fe2d46d9e3749
justin1dennison <justin1dennison@gmail.com>
2015-09-28 17:36:26 -0400

    Created blank stubs with comments for students

--- Chapter 2 - ng2Du App/js/app.js
+++ Chapter 2 - ng2Du App/js/app.js
@@ -1,26 +1,21 @@
 // Let's create our application called ng2du
-var app = angular.module("ng2du", []);
-//Let's add a controller for our application that manages the HomePage called HomeController
-app.controller("HomeController", function($scope){
 
-});
+//Let's add a controller for our application that manages the HomePage called HomeController
+
+
   //We currently have nothing going on, but we you never know
 
 //Let's add a controller for our application that manages the Todos called the TodoController
-app.controller("TodoController", function($scope){
-  $scope.todos = [];
-  $scope.title = "";
-  $scope.description = "";
-  $scope.id =
 
+  //Let's create an array called todos
 
-  $scope.addTodo = function(){
-    $scope.todos.push({title: $scope.title, description: $scope.description});
-  };
+  //Let's create two properties called description and title
 
-  $scope.removeTodo = function(index){
-    $scope.todos.splice(index,1);
-  };
-});
-  //Let's add create a placeholder to our todos --> array
-  //Let's create references for the form to build a todo
+  // Let's figure out how we can add a todo to the scope.
+ 
+
+  //We need to be able to remove the todos as we complete them
+  
+    
+ 
+  
